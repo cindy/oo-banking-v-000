75ea51a45f99ac6059b6cf89113f335b514ad69d
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-09 04:13:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,8 @@
 class Transfer
-  def initialize(name, account, amount)
+  attr_reader(:sender, :account, :amount)
+  def initialize(sender, account, amount)
+    @sender = sender
+    @account = account
+    @amount = amount
   end
 end
